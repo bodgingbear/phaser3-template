bc38ba575c82523a0e98355ff670d42e3d4e5631
Bartosz Legięć <bartosz@legiec.io>
2022-02-13 23:58:00 +0100

    Update phaser

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -6,12 +6,13 @@ export class GameScene extends Phaser.Scene {
   }
 
   public create(): void {
-    const text = this.add.text(1280 / 2, 720 / 2, 'Here is the game', {
-      fontSize: '48px',
-      fill: '#fff',
-      align: 'center',
-      lineSpacing: 10,
-    });
+    const text = this.add
+      .text(1280 / 2, 720 / 2, 'Here is the game', {
+        fontSize: '48px',
+        color: '#fff',
+        align: 'center',
+      })
+      .setLineSpacing(10);
 
     text.setOrigin(0.5, 0.5);
   }
